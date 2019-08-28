c77b0f8dc0853b6d8a3cad56078619a631c2adb1
baines95 <tinhq16@gmail.com>
2019-08-28 10:36:59 +0700

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <p>Chào xìn</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
